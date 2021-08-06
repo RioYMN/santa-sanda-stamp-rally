b9cb1260c9a3af0983908c3223eb47c7952f098f
ozono8810 <ozono8810@gmail.com>
2021-08-06 16:11:09 +0900

    エンティティの追加

---
+++ src/main/java/jp/kobespiral/santasandastamprally/entity/TaskLog.java
@@ -0,0 +1,49 @@
+package jp.kobespiral.santasandastamprally.entity;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * タスクログのエンティティ
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class TaskLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_log_id", nullable = false)
+    Long taskLogId; // タスクログID
+
+    @Column(name = "task_id", nullable = false)
+    Long taskId; // タスクID
+
+    @Column(name = "user_id", nullable = false)
+    UUID userId; // ユーザID
+    
+    @Column(name = "is_correct", nullable = true)
+    Boolean isCorrect; // 正解フラグ
+
+    @Column(name = "answer", nullable = true)
+    String answer; // 回答
+
+    @Column(name = "created_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date createdAt; // 作成日時
+
+    @Column(name = "created_by", nullable = false)
+    String createdBy; // 作成者
+}
