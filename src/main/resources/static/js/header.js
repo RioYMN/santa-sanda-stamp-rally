d8f84141b13dbb55232dd504168451b8fffbc975
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2021-08-06 18:19:30 +0900

    headder,footerを追加

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,6 @@
+$("#header").append("<nav id='header-nav'> header");
+$("#header-nav").append("<ul>");
+$("#header-nav ul").append("<li><a href=''> Home");
+$("#header-nav ul").append("<li><a href=''> 応募");
+$("#header-nav ul").append("<li><a href=''> 賞品");
+$("#header-nav ul").append("<li><a href=''> お問い合わせ");
