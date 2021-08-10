90366552d5505200b32b7b1103420b86904e068c
ozono8810 <ozono8810@gmail.com>
2021-08-10 10:43:07 +0900

    管理者のエンティティを追加

---
+++ src/main/java/jp/kobespiral/santasandastamprally/entity/Admin.java
@@ -0,0 +1,50 @@
+package jp.kobespiral.santasandastamprally.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 管理者のエンティティ
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Admin {
+    @Column(name="admin_id", nullable=false)
+    @Id
+    Long adminId;   //管理者ID
+
+    @Column(name="name", nullable=true)
+    @NotBlank
+    String name; // 名前
+
+    @Column(name="last_login_at", nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date lastLoginAt; // 最終ログイン日時
+
+    @Column(name="created_at", nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date createdAt; // 作成日時
+
+    @Column(name="updated_at", nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date updatedAt; // 最終更新日時
+
+    @Column(name="deleted_at", nullable=true)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date deletedAt; // 削除日時
+
+    @Column(name="deleted_by", nullable=true)
+    Long deletedBy; // 削除者
+}
