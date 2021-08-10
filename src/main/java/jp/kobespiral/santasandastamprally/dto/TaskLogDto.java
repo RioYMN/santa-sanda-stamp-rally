c45302d2c8bc170e058f8b47e626f4c20b7a41b5
ozono8810 <ozono8810@gmail.com>
2021-08-10 09:49:14 +0900

    タスクログのDTOを追加

---
+++ src/main/java/jp/kobespiral/santasandastamprally/dto/TaskLogDto.java
@@ -0,0 +1,35 @@
+package jp.kobespiral.santasandastamprally.dto;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import jp.kobespiral.santasandastamprally.entity.TaskLog;
+import lombok.Data;
+
+@Data
+public class TaskLogDto {
+    @NotBlank
+    Long taskId;
+
+    @NotBlank
+    UUID userId;
+
+    @NotNull
+    boolean isCorrect;
+
+    @NotNull
+    String answer;
+
+    public TaskLog toEntity() {
+        TaskLog taskLog = new TaskLog();
+        taskLog.setTaskId(taskId);
+        taskLog.setUserId(userId);
+        taskLog.setIsCorrect(isCorrect);
+        taskLog.setAnswer(answer);
+        taskLog.setCreatedAt(new Date());
+        return taskLog;
+    }
+}
